6ae25d684bf546d7877e513c5f052dfe5e90550b
coffee <1coffeegang1@gmail.com>
2026-08-29 10:39:50 -0400

    Update kittynoises.js

--- kittynoises.js
+++ kittynoises.js
@@ -8,15 +8,19 @@ function getRandomInt(min, max) {
   	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const meow1 = ["meow!", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
+const meow1 = ["meow!", "meooow...", ":3", ">_<", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
 
-let meow2 = ["meow!", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
+let meow2 = ["meow!", "meooow...", ":3", ">_<", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
 
-let meow3 = ["meow!", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
+let meow3 = ["meow!", "meooow...", ":3", ">_<", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
 
-let meow4 = ["meow!", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
+let meow4 = ["meow!", "meooow...", ":3", ">_<", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
 
-let meow5 = ["meow!", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
+let meow5 = ["meow!", "meooow...", ":3", ">_<", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
+
+let meow6 = ["meow!", "meooow...", ":3", ">_<", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
+
+let meow7 = ["meow!", "meooow...", ":3", ">_<", "mrreoow", "mrrap", "mrmfmrrr :3", "nnamsajmsahjasm", "mfrmmrmrmfph", "rreeoow", "owo", "meeow", "mrmrmrfmr", "nya", "mrrrp", "mrmrmrr", "nyaaaaa", "rawwwwranyaayawm", "bweh >w<", "uwu", "ouo", ">///<", "awawa", "awa...", "mrawrmwarm", "meooowwwe", "meowewre", "nyan!", "nyanyanyan~", "kehehe :3", "uwaa ><", "wawa!", "awuuuwa.."];
 
 generateStory();
 
@@ -45,12 +49,27 @@ function newMeow5(){
     
     document.getElementById("meowspeak5").innerHTML = ` ${meowspeak5}`;   
 }
+function newMeow6(){
+    let meowspeak5 = meow6[getRandomInt(0, meow6.length)];
+    
+    document.getElementById("meowspeak6").innerHTML = ` ${meowspeak5}`;   
+}
+function newMeow7(){
+    let meowspeak5 = meow7[getRandomInt(0, meow7.length)];
+    
+    document.getElementById("meowspeak7").innerHTML = ` ${meowspeak5}`;   
+}
+
 
 function generateStory(){
     newMeow2();
     newMeow1();
     newMeow3();
     newMeow4();
+    newMeow5();
+    newMeow5();
+    newMeow6();
+    newMeow7();
 }
 
 
